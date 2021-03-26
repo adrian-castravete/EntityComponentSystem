322d5cb0261824a86026a31861d81066f37af386
Adrian Castravete <adrian.castravete@gmail.com>
2021-03-26 10:00:44 +0000

    Add licence and ensure working version

--- main.lua
+++ main.lua
@@ -6,11 +6,11 @@ fkge.game {
   height = 192,
 }
 
-e.scene('game', function() {
-})
+e.scene('game', function()
+end)
 
-e.scene('load', function() {
+e.scene('load', function()
   fkge.scene'game'
-})
+end)
 
 e.scene'load'
